c405a69be43c2eefe463e0f9abef22608b12c13b
mtsikevich <tumelo.motsikelane@gmail.com>
2026-05-11 18:34:37 +0200

    move apphost project

--- Vouchitory.AppHost/AppHost.cs
+++ Vouchitory.AppHost/AppHost.cs
@@ -1,6 +1,9 @@
+using Aspire.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 var messageQueue = builder.AddLavinMQ("message-queue");
-builder.AddProject<Projects.Vouchitory_Api>("vouchitory-api")
+var api = builder.AddProject<Projects.Vouchitory_Api>("vouchitory-api")
     .WithReference(messageQueue)
     .WaitFor(messageQueue);
+
 builder.Build().Run();
